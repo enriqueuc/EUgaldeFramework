4c845b64ded8b4e8fb1e75e575df44f56f1eac7b
Enrique Ugalde <enrique@ekt1136648mac.local>
2017-04-12 10:33:29 -0500

    - Working Framework

--- EUgaldeFramework/strings.h
+++ EUgaldeFramework/strings.h
@@ -1,9 +1,15 @@
 //
-//  EUgaldeFramework.h
+//  strings.h
 //  EUgaldeFramework
 //
 //  Created by Enrique Ugalde on 12/04/17.
 //  Copyright © 2017 Enrique Ugalde. All rights reserved.
 //
 
-#import <EUgaldeFramework/strings.h>
+#import <Foundation/Foundation.h>
+
+@interface strings : NSObject
+
++(NSString *)stringExample;
+
+@end
---
+++ EUgaldeFramework/strings.m
@@ -0,0 +1,18 @@
+//
+//  strings.m
+//  EUgaldeFramework
+//
+//  Created by Enrique Ugalde on 12/04/17.
+//  Copyright © 2017 Enrique Ugalde. All rights reserved.
+//
+
+#import "strings.h"
+
+@implementation strings
+
++(NSString *)stringExample
+{
+    return @"getted from my Framework";
+}
+
+@end
